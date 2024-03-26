074eaf634872431a58fe785d03d20419bc5ea9fc
Josh Day <joshday@monzo.com>
2024-03-26 14:35:38 +0000

    Added validity checking for posts and user signups

--- api/src/controllers/posts.go
+++ api/src/controllers/posts.go
@@ -53,8 +53,8 @@ func CreatePost(ctx *gin.Context) {
 	if len(requestBody.Message) == 0 {
 		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Post message empty"})
 		return
-	} else if len(requestBody.Message) > 10 {
-		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Post message too long (must be less than INSERT HERE)"})
+	} else if len(requestBody.Message) > 280 {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Post message too long (must be less than or equal to 280 chars)"})
 		return
 	}
 
