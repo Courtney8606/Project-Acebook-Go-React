0bedc4ee20264051f501be138425ece0efd3d2ed
Patrick Skipworth <pskipworth@fastmail.com>
2024-03-27 11:58:45 +0000

    Navigation bar basic signed in / signed out render
    Move all signout button functionality to navigation bar. Navigation bar renders dependent on sign-in station with appropriate buttons.

--- frontend/src/components/NavigationBar/NavigationBar.jsx
+++ frontend/src/components/NavigationBar/NavigationBar.jsx
@@ -1,18 +1,38 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 import "./NavigationBar.css";
 
 // NavigationBar will render at the top of the page. The <nav>
 // tag identifies it as the navigation information for the site
 const NavigationBar = () => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  const logout = () => {
+    // Removes the stored login token from browser's localStorage
+    localStorage.removeItem('token')
+    navigate("/login");
+  };
+
+  const login = () => {
+    navigate('/login')
+  };
+
+  const signup = () => {
+    navigate('/signup')
+  };
+
+if (token != null) {
   return (
-    <header>
-      <nav>
-        ACEBOOK <Link to="/login">Login</Link>  <Link to="/signup">Signup</Link>
-        <div className="line"></div>
-      </nav>
-    </header>
-  );
+    <button onClick={logout}>Logout</button>
+  )}
+
+else if (token === null) {
+  return (
+    <div>
+    <button onClick={login}>Login</button>
+    <button onClick={signup}>Signup</button>
+    </div>
+  )}
 };
 
 export default NavigationBar;
