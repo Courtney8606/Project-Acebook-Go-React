357a196f971a4975dc6521c41cb543313b10e5ee
Rachel Turrell <rachelturrell@gmail.com>
2024-03-28 11:11:36 +0000

    make services signup function for errors

--- frontend/src/pages/Signup/SignupPage.jsx
+++ frontend/src/pages/Signup/SignupPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "/src/Signup.css";
 import { signup } from "../../services/authentication";
+import { signupErrors } from "../../services/signup";
 
 export const SignupPage = () => {
   const [email, setEmail] = useState("");
@@ -16,33 +17,34 @@ export const SignupPage = () => {
 
     const handleSubmit = async (event) => {
       event.preventDefault();
-      let data;
+      // let data;
       try {
           const response = await signup(email, password);
-          data = await response.json()
           console.log(response)
-          if (response.status == 400)
-          {
-            if (data.message == "Must supply username and password") {
-              setErrorMessage("Please input both a valid email address and a password");
-            }
+          setErrorMessage(signupErrors)
+          // if (response.status == 400)
+          // {
+          //   if (data.message == "Must supply username and password") {
+          //     setErrorMessage("Please input both a valid email address and a password");
+          //   }
             
-            else if (data.message == "Invalid email address") {
-                setErrorMessage("Please include a valid email address");
-                clearFormFields();
+          //   else if (data.message == "Invalid email address") {
+          //       setErrorMessage("Please include a valid email address");
+          //       clearFormFields();
               
-            }
-            else if (data.message == "Email address already in use") {
-              setErrorMessage("This email address is already in use. Please log in or sign up with different email address");
-              document.getElementById("password").value = "";
-            }
-            else {setErrorMessage("An error has occurred. Please try again");
-                  clearFormFields();
-                  }
+          //   }
+          //   else if (data.message == "Email address already in use") {
+          //     setErrorMessage("This email address is already in use. Please log in or sign up with different email address");
+          //     document.getElementById("password").value = "";
+          //   }
+          //   else {setErrorMessage("An error has occurred. Please try again");
+          //         clearFormFields();
+          //         }
+          // }
+          // else{console.log("redirecting...:");
+          // navigate("/login")}
           }
-          else{console.log("redirecting...:");
-          navigate("/login")}
-          }
+
         catch (err) {
         console.error(err);
         navigate("/posts");
