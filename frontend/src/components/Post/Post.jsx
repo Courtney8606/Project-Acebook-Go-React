ddffd89bdd3230ed89b1e2bd4d9081ceda6ad656
Patrick Skipworth <pskipworth@fastmail.com>
2024-04-03 15:48:45 +0100

    Username beside posts
    Show the posters username beside their posts in the feed.

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -3,12 +3,20 @@ import "/src/Post.css";
 const Post = (props) => {
   return (
     <div className="post">
-      <div className="icon-posts">
-        <i className="fas fa-user-circle"></i>
+      <div className='profilePicture'>
+        <div className="icon-posts">
+          <i className="fas fa-user-circle"></i>
+        </div>
       </div>
-      <article className="feed-message" key={props.post._id}>
-        {props.post.message}
-      </article>
+      <div className='message-box'>
+        <div className='userinfo'>
+            {/* <p>On {props.post.created_at}</p> */}
+            <p>{props.post.username} said:</p>
+          </div>
+        <article className="feed-message" key={props.post._id}>
+          {props.post.message}
+        </article>
+      </div>
     </div>
   );
 };
