49571a22809bd19a37f934c153409b17b31da180
Patrick Skipworth <pskipworth@fastmail.com>
2024-04-07 14:01:55 +0100

    FeedPage.jsx refactoring
    Removed DeleteButton import from FeedPage.jsx (not used on page)

--- frontend/src/pages/Feed/FeedPage.jsx
+++ frontend/src/pages/Feed/FeedPage.jsx
@@ -5,7 +5,6 @@ import { getComments, commentCreate } from "../../services/comments";
 import Post from "../../components/Post/Post";
 import UserComment from "../../components/Comment/Comment";
 import LikeButton from "../../components/LikeButton/LikeButton";
-import DeleteButton from "../../components/DeleteButton/DeleteButton";
 import CommentsBox from "../../components/CommentsBox/CommentsBox";
 import { getLikes, likeCreate, unlikeCreate } from "../../services/likes";
 import "/src/FeedPage.css";
